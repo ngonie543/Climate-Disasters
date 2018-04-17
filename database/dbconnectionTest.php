b16074de979592ae36a67c417d03b8cfaeb56d8d
Ngoni Justin Ndhlala <ngoni.ndhlala@duke.edu>
2018-04-17 12:06:21 -0400

    Add files via upload
    file used to test connection between php and database

---
+++ database/dbconnectionTest.php
@@ -0,0 +1,12 @@
+
+
+
+<?php
+
+    define("DB_SERVER", "localhost");
+    define("DB_USERNAME", "root");
+    define("DB_PASSWORD", "root");
+    define("DB_DATABASE", "climate_disasters_db");
+    $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+?>
